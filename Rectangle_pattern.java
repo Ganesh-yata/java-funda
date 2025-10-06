8bf47dd83ecb92dc34b4c375cc9eaaee64bf692c
Ganesh-yata <ganeshyata75@gmail.com>
2025-10-06 23:14:18 +0530

    Rectangle pattern

---
+++ Rectangle_pattern.java
@@ -0,0 +1,15 @@
+public class Rectangle_pattern {
+  public static void main(String[] args) {
+    int n = 4;
+    int m = 10;
+    // outer loop 
+    for (int i = 1 ; i<=n ; i++){
+      //inner loop 
+      for (int j = 1 ; j<m ; j++){
+        System.out.print("*");
+      }
+      System.out.println("*");
+    }
+
+  }
+}
