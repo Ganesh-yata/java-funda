3e6a35424d916e0fc5a5195ec0feeb1a8ab4e66d
Ganesh-yata <ganeshyata75@gmail.com>
2025-10-23 23:08:01 +0530

    palindrome

---
+++ PalindromeNumber.java
@@ -0,0 +1,19 @@
+class PalindromeNumber 
+{
+public static void main(String[] args)
+{
+    String str = "mam", reverseStr = "";
+    for(int i =(str.length() - 1);i>=0;--i)
+    {
+        reverseStr = reverseStr + str.charAt(i);
+
+    }
+    if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
+      System.out.println(str + " is a Palindrome String.");
+    }
+    else {
+      System.out.println(str + " is not a Palindrome String.");
+    }
+  }
+}
+
